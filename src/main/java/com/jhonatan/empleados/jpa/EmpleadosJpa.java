9f66fd2090dc1c40a8b2ca404f942f0ce128360b
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-08-01 21:14:50 -0500

    class Empleadosjpa: modificando una persona

--- src/main/java/com/jhonatan/empleados/jpa/EmpleadosJpa.java
+++ src/main/java/com/jhonatan/empleados/jpa/EmpleadosJpa.java
@@ -7,7 +7,7 @@ public class EmpleadosJpa {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         TbPersonaDao p1 = new TbPersonaDao();
-        String m = p1.actualizarPersona(6, "Eli", "Panta", 45, "912331117");
+        String m = p1.actualizarPersona(3, "Maria", "Ojeda", 45, "955331117");
         System.out.println(m);
     }
 }
