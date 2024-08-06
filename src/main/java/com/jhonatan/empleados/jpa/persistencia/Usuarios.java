48b61a1ff9161fc9e640e1881008d5394c21457d
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-08-06 16:44:52 -0500

    nueva entidad con su respectivo controlador

---
+++ src/main/java/com/jhonatan/empleados/jpa/persistencia/Usuarios.java
@@ -0,0 +1,73 @@
+package com.jhonatan.empleados.jpa.persistencia;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+@NamedQueries({
+    @NamedQuery(name = "Usuarios.findAll", query = "SELECT u FROM Usuarios u"),
+    @NamedQuery(name = "Usuarios.findByIdusuario", query = "SELECT u FROM Usuarios u WHERE u.idusuario = :idusuario"),
+    @NamedQuery(name = "Usuarios.findByUsuario", query = "SELECT u FROM Usuarios u WHERE u.usuario = :usuario"),
+    @NamedQuery(name = "Usuarios.findByContrasenia", query = "SELECT u FROM Usuarios u WHERE u.contrasenia = :contrasenia")})
+public class Usuarios implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "idusuario")
+    private Integer idusuario;
+    @Basic(optional = false)
+    @Column(name = "usuario")
+    private String usuario;
+    @Basic(optional = false)
+    @Column(name = "contrasenia")
+    private String contrasenia;
+
+    public Usuarios() {
+    }
+
+    public Usuarios(Integer idusuario) {
+        this.idusuario = idusuario;
+    }
+
+    public Usuarios(Integer idusuario, String usuario, String contrasenia) {
+        this.idusuario = idusuario;
+        this.usuario = usuario;
+        this.contrasenia = contrasenia;
+    }
+
+    public Integer getIdusuario() {
+        return idusuario;
+    }
+
+    public void setIdusuario(Integer idusuario) {
+        this.idusuario = idusuario;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+}
