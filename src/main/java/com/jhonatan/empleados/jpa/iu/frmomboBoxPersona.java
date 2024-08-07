193c95b289f9436fe6bf6da2d21d3abf8fa405de
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-08-07 09:34:27 -0500

    FORMULARIO frmomboBoxPersona: método para cargar los datos en el comobo box

--- src/main/java/com/jhonatan/empleados/jpa/iu/frmomboBoxPersona.java
+++ src/main/java/com/jhonatan/empleados/jpa/iu/frmomboBoxPersona.java
@@ -1,15 +1,22 @@
 package com.jhonatan.empleados.jpa.iu;
 
 import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatMaterialLighterIJTheme;
+import com.jhonatan.empleados.jpa.logica.TbPersonaDao;
+import com.jhonatan.empleados.jpa.logica.UsuarioDao;
+import com.jhonatan.empleados.jpa.persistencia.Tbpersona;
+import javax.swing.JComboBox;
 
 public class frmomboBoxPersona extends javax.swing.JFrame {
-
+    
+    TbPersonaDao persona = new TbPersonaDao();
+    
     public frmomboBoxPersona() {
         initComponents();
+        this.setLocationRelativeTo(null);
         FlatMaterialLighterIJTheme.setup();
-
+        this.cbxPersona(cbxPersonas);
     }
-
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -29,7 +36,7 @@ public class frmomboBoxPersona extends javax.swing.JFrame {
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText("Carga de datos desde un Combo Box");
 
-        cbxPersonas.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        cbxPersonas.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -65,11 +72,11 @@ public class frmomboBoxPersona extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     public static void main(String args[]) {
-
+        
         FlatMaterialLighterIJTheme.setup();
-
+        
         java.awt.EventQueue.invokeLater(new Runnable() {
-
+            
             public void run() {
                 new frmomboBoxPersona().setVisible(true);
             }
@@ -78,9 +85,13 @@ public class frmomboBoxPersona extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JComboBox<String> cbxPersonas;
+    private javax.swing.JComboBox<Tbpersona> cbxPersonas;
     private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
+
+    private void cbxPersona(JComboBox cbxPersona) {
+        persona.getPersonasCMB(cbxPersonas);
+    }
 }
