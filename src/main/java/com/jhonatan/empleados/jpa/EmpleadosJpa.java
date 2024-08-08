d3583c75c37d65bd39c526044a6c4ab8581430aa
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-08-08 17:50:36 -0500

    formulario Persona: agregando un nuevo metodo para dirigirnos al formulario donde se lista los registros en un comboBox

--- src/main/java/com/jhonatan/empleados/jpa/EmpleadosJpa.java
+++ src/main/java/com/jhonatan/empleados/jpa/EmpleadosJpa.java
@@ -1,11 +1,29 @@
 package com.jhonatan.empleados.jpa;
 
+import com.jhonatan.empleados.jpa.iu.frmLogin;
 import com.jhonatan.empleados.jpa.iu.frmPersona;
+import com.jhonatan.empleados.jpa.iu.frmomboBoxPersona;
 
 public class EmpleadosJpa {
-    
+
     public static void main(String[] args) {
-        frmPersona  ven = new frmPersona();
+        d();
+        //a();
+    }
+
+    public static void d() {
+        frmLogin d = new frmLogin();
+        d.setVisible(true);
+    }
+
+    public static void g() {
+        frmPersona ven = new frmPersona();
         ven.setVisible(true);
     }
+
+    public static void a() {
+        frmomboBoxPersona ven = new frmomboBoxPersona();
+        ven.setVisible(true);
+    }
+
 }
